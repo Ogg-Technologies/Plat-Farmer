2783c5e2ec2a233f6f1d5619a585d2b741f5dd29
unknown <ogg.developer@gmail.com>
2018-12-02 13:01:42 +0100

    Added python script to fetch new data.

---
+++ updateData.py
@@ -0,0 +1,31 @@
+import json
+import urllib.request
+
+
+primes_url = "http://www.oggtechnologies.com/api/ducatsorplat/v2/ItemPriceData.json"
+missions_url = "http://www.oggtechnologies.com/api/lootr/v2/rewards.json"
+
+
+def download_json(url):
+    with urllib.request.urlopen(url) as req:
+        data = json.loads(req.read().decode())
+    return data
+
+
+def write_data_as_js(file_name, data):
+    with open("data/"+file_name, "w") as f:
+        data_str = "var primesDataJson = "
+        data_str += json.dumps(data)
+        f.write(data_str)
+
+
+# Download the primes
+primes_json = download_json(primes_url)
+primes_json = primes_json["data"]
+# Download the mission
+missions_json = download_json(missions_url)
+
+# Write the primes
+write_data_as_js("primesDataJson.js", primes_json)
+# Write the missions
+write_data_as_js("missionsDataJson.js", missions_json)
