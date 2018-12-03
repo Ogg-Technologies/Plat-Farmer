6672344adc110aa3a1c8d4899ff3a82a9c5b534d
unknown <ogg.developer@gmail.com>
2018-12-03 18:05:58 +0100

    Added prime plat price calculation

--- script.js
+++ script.js
@@ -1,12 +1,49 @@
 var wm = new Vue({
     el: '#app',
     data: {
-        currentTab: "missions",
+        currentTab: "primes",
         missions: missionsDataJson,
         relics: [],
         primes: [],
     },
+    computed:{
+        displayedMissions: function(){
+
+        },
+        displayedRelics: function(){
+
+        },
+        displayedPrimes: function(){
+            let primesToShow = []
+            for (comp of this.primes){
+                comp.plat = this.calculatePlatPrice(comp);
+                primesToShow.push(comp);
+            }
+            primesToShow.sort(function(a, b){
+                return b.plat-a.plat;
+            });
+            primesToShow = primesToShow.slice(0, 10);
+            return primesToShow;
+        },
+    },
     methods: {
+        calculatePlatPrice: function(comp){
+            function getAveragePrice(ordersList){
+                // Uses a list of warframe.market orders to calculate an
+                // average price. Returns the average price of that list
+                let sum = 0;
+                for (let i = 0; i<Math.min(ordersList.length, 3); i++){
+                    order = ordersList[i];
+                    sum += order.Price;
+                }
+                return sum/3
+            }
+            let ordersData = comp.Data["Warframe.market"].pc;
+            let buyPrice = getAveragePrice(ordersData.OnlineBuyers);
+            let sellPrice = getAveragePrice(ordersData.OnlineSellers);
+            let plat = (buyPrice+sellPrice)/2
+            return plat;
+        },
         tabClicked(tabName) {
             this.currentTab = tabName;
         },
@@ -35,6 +72,11 @@ var wm = new Vue({
                 }
             }
         }
+
+        // Remove all the relics from the missionsList
+        this.missions = this.missions.filter(function(entry){
+            return entry.type != "Relic"
+        })
 
     },
 })
